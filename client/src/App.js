98f6548d7722f6476973718f962943cb4eec257c
RitSar <55022926+RitSar@users.noreply.github.com>
2020-10-19 13:48:50 +0530

    routed home, register, login pages and navbar with semantic-ui styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react'
 import './App.css';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there!</h1>
-    </div>
+    <Router>
+      <Container>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
   );
 }
 
